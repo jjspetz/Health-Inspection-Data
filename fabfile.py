7e87d05c21be13a1aa815476897db909a7bc269c
JJ <jj.spetseris@yahoo.com>
2017-05-25 16:09:25 -0500

    trying to work on db

---
+++ fabfile.py
@@ -0,0 +1,27 @@
+from fabric.api import run, env, sudo, cd, prefix
+
+env.hosts = ['45.32.192.195']
+env.user = 'jjspetz'
+
+DIR = '/home/jjspetz/healthinspections'
+VENV = 'source /home/jjspetz/.virtualenvs/dj-exercise/bin/activate && source SECRETS.ENV'
+
+def start ():
+  with cd(DIR):
+    with prefix(VENV):
+      run('pm2 start uwsgi -- --ini uwsgi.ini > start.log')
+
+def stop ():
+  run('pm2 stop all > stop.log')
+
+def deploy ():
+  with cd(DIR):
+    run('git pull')
+
+    with prefix(VENV):
+      run('pip install -r requirements.txt  > install.log')
+
+    run('pm2 restart all > restart.log')
+
+def hello ():
+  print("Hello")
