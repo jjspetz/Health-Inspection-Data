6621d265d88cb910ebe08bf3d740f34692caa084
JJ <jj.spetseris@yahoo.com>
2017-06-02 10:51:10 -0500

    changes the style of the page

---
+++ inspectData/static/JS/main.js
@@ -0,0 +1,5 @@
+// background stuff
+var index = Math.floor(Math.random()*12);
+document.body.style.background =
+"#56d879 url('/static/img/b"+index+".jpg') no-repeat fixed center";
+document.body.style.backgroundSize = "cover";
